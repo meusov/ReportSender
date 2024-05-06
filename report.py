0dad9a7d4361f04b7f642f82c38ac6c5b5e3e16e
Dmitry Usov <127579407+meusov@users.noreply.github.com>
2024-05-06 22:37:49 +0300

    Update report.py

--- report.py
+++ report.py
@@ -2,22 +2,21 @@ import time
 import subprocess
 import platform
 import socket
+import logging
+import os
+import random
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import logging
-import os
-import random
 
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 
 def check_domain_availability(domain, processed_domains):
     if domain in processed_domains:
         return False
-
     try:
         socket.gethostbyname(domain)
         return True
@@ -32,32 +31,19 @@ def submit_report(driver, domain, processed_domains):
                 canceled_domains_file.write(f'{domain}\n')
                 processed_domains.add(domain)
         return False
-
     driver.get('https://safebrowsing.google.com/safebrowsing/report_error/?hl=en')
-
     url_field = driver.find_element(By.ID, 'url')
     url_field.clear()
     url_field.send_keys(domain)
-
     additional_details_field = driver.find_element(By.ID, 'dq')
-    
     additional_details_texts = [
-        '''
-        text1
-        ''',
-        '''
-        text2
-        '''
-          ,
-        '''
-        text3
-        '''
+        '''text1''',
+        '''text2''',
+        '''text3'''
     ]
-    
     additional_details_text = random.choice(additional_details_texts)
     additional_details_field.clear()
     additional_details_field.send_keys(additional_details_text)
-    
     try:
         WebDriverWait(driver, 100).until(EC.url_contains('submit_success'))
         logging.info(f'Отчет для: {domain} успешно отправлен!')
@@ -66,7 +52,6 @@ def submit_report(driver, domain, processed_domains):
         logging.warning(f'Не удалось перейти на страницу успешной отправки отчета для {domain}.')
         return False
 
-# Открываем файл с доменами
 with open('domains.txt', 'r') as f:
     domains = f.read().splitlines()
 
@@ -74,27 +59,20 @@ processed_domains = set()
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-
 service = webdriver.chrome.service.Service(ChromeDriverManager().install())
-
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 max_attempts = 3
-
 index = 0  
 
 try:
-
     with open('successful.txt', 'a') as successful_domains_file:
         while index < len(domains):
             domain = domains[index]
             attempts = 0 
-
             while attempts < max_attempts:
                 success = submit_report(driver, domain, processed_domains)
-
                 if success:
-            
                     current_time = time.strftime('%Y-%m-%d %H:%M:%S')
                     successful_domains_file.write(f'Домен: {domain} - репорт успешно отправлен! Дата: {current_time}\n')
                     successful_domains_file.flush()
@@ -102,14 +80,10 @@ try:
                 else:
                     attempts += 1
                     logging.warning(f'Повторяем попытку отправки для {domain} (попытка {attempts})...')
-
             if attempts >= max_attempts:
                 logging.error(f'Зафейлилась отправка для домена {domain} после {max_attempts} попыток.')
-
             time.sleep(2)
-
             index += 1 
-
 except TimeoutException as e:
     logging.error(f'Произошла ошибка TimeoutException: {str(e)}')
 except Exception as e:
